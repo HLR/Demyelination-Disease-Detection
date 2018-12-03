4bec6fca5ada6dec13b004e21e3dc28d6d87dc13
Grant Collins <grollins97@gmail.com>
2018-12-02 19:22:16 -0600

    created csv processing file for modularity

---
+++ csv_processors.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  2 19:20:27 2018
+
+@author: gscollins, johykim, Shees Ahmed
+
+Purpose: This file contains functions that read in our csv files for processing.
+"""
+
