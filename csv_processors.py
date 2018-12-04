84957d0acd3a5af90b258fd08ed6fd51c078f9ba
gscollins <44511904+gscollins@users.noreply.github.com>
2018-12-04 12:54:35 -0600

    Add files via upload

--- csv_processors.py
+++ csv_processors.py
@@ -8,3 +8,24 @@ Created on Sun Dec  2 19:20:27 2018
 Purpose: This file contains functions that read in our csv files for processing.
 """
 
+import os
+import pandas
+import numpy as np
+
+script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+data_folder_path = os.path.join(script_dir, "Data Files")
+# ^ "Data Files" is the folder we keep all our training csvs in
+
+
+def wave10reader(fileName):
+    with open(os.path.join(data_folder_path, fileName), 'r', ) as source:
+        return pandas.read_csv(source,index_col=0)
+        
+def codyReader(fileName):
+    with open(os.path.join(data_folder_path, fileName), 'r') as f:
+        #creates the pandas csv_reader-generated DataFrame
+        reader = pandas.read_csv(f, sep = None, engine = 'python', header = None)
+        reader = reader.values
+    rows1= np.array(reader)
+    returner = rows1[:,1:len(rows1[0])-2]
+    return returner.astype(float).tolist()
